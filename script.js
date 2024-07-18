4b34e50089edac3d81a461b0cc40a1d03a589986
Nate Patton <170039653+NatePatton-Dev@users.noreply.github.com>
2024-07-18 09:38:33 -0500

    add button to resize grid

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
 const container = document.querySelector(".container");
 
 // create 16x16 grid of square divs
-let N = 16
-for (let i = 0; i < N; i++) {
-    //create div row container
-    let row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    //16x loop to create 16 divs in row container
-    for (let j = 0; j < N; j++) {
-        let column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
+let gridNumber = 16
+
+function createGrid () {
+    for (let i = 0; i < gridNumber; i++) {
+        //create div row container
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        //16x loop to create 16 divs in row container
+        for (let j = 0; j < gridNumber; j++) {
+            let column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        };
     };
 };
 
-let squares = document.querySelectorAll(".column");
+createGrid();
 
-squares.forEach(function (square) {
-    function draw() {
-        square.classList.remove("column");
-        square.classList.add("columnHover");
-    }
-    square.addEventListener("mouseover", draw);
-});
+function addDrawEventListener () {
+    let squares = document.querySelectorAll(".column");
+    squares.forEach(function (square) {
+        function draw() {
+            square.classList.remove("column");
+            square.classList.add("columnHover");
+        };
+        square.addEventListener("mouseover", draw);
+    });
+};
 
-squares.forEach(function (square) {
-    function draw() {
-        square.classList.remove("columnHover");
-        square.classList.add("column");
-    }
-    square.addEventListener("mouseout", draw);
-});
+addDrawEventListener();
 
-let newGrid = document.querySelector("#newGrid")
+// squares.forEach(function (square) {
+//     function draw() {
+//         square.classList.remove("columnHover");
+//         square.classList.add("column");
+//     }
+//     square.addEventListener("mouseout", draw);
+// });
+
+let newGrid = document.querySelector("#newGrid");
+
 newGrid.addEventListener("click", function() {
-    let gridNumber = prompt("Number of squares per side of new grid?")
-    //if more than 100 re-prompt
-
+    function getGridNumber () {
+        gridNumber = +prompt("Number of squares per side of new grid?");
+        //if more than 100 re-prompt
+        if (gridNumber > 100) {
+            getGridNumber();
+        } else if (!gridNumber) {
+            getGridNumber();
+        };
+    };
+    getGridNumber();
+    function removeGrid (parent) {
+        while (parent.firstChild) {
+            parent.lastChild.remove();
+        };
+    };
+    removeGrid(container);
+    createGrid();
+    addDrawEventListener();
 });
 
-//adding button made total page height more than window height
-//flex shrink doesn't appear to be working
+// how to fit grid and button to window size?
